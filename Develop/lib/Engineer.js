46d66f09b16993cb0b69247d385c3bbf1e297ec1
Hayden Rust <haydenrust1@gmail.com>
2020-09-15 19:49:17 -0700

    created inquirer function with question var

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,19 @@
 const Employee = require('./Employee');
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const employee = require('./Employee');
 
-const engineer = new Employee
+class Engineer extends Employee {
+    constructor(gitUsername) {
+        // might need this?
+        super(name, id, email)
+        this.gitUsername = gitUsername;
+    }
+
+    getGithub() {
+        console.log(`${this.gitUsername}`);
+        return this.gitUsername
+    }
+}
+
+const engineer = new Engineer('haydenrust1')
+engineer.getGithub();
+engineer.getRole();
