b2c0a63805a1a974bda0e210def779d3b5cda14f
Hayden Rust <haydenrust1@gmail.com>
2020-09-15 17:46:00 -0700

    starting files added

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,5 @@
+const Employee = require('./Employee');
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const employee = require('./Employee');
+
+const engineer = new Employee
